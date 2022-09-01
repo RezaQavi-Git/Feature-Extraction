69472845ef2cc65a22dc6668ee0d8eebbc8dfdae
Reza Qavi <rezaqavi1378@gmail.com>
2022-09-01 15:37:08 +0430

    MACD done

---
+++ indicators/macd.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import pandas_ta as ta
+import matplotlib.pyplot as plt
+import matplotlib.dates as mpl_dates
+import matplotlib.pyplot as plt
+
+from utils import *
+from utils.basic import create_dataframe
+from utils.cross_line import cross_line_bearish, cross_line_bullish, cross_line_from_above, cross_line_from_bottom
+from utils.cross_value import cross_value_from_above, cross_value_from_bottom
+from utils.difference_from_line import difference_from_line
+from utils.difference_from_value import difference_from_value
+from utils.sum_in_period import *
+from utils.up_down import up_down_line
+
+
+
+def macd(df):
+    macd_value = ta.macd(df['close'])
+
+    value_cross_0_above = cross_value_from_above(macd_value['MACD_12_26_9'], 0)
+    value_cross_0_bottom = cross_value_from_bottom(macd_value['MACD_12_26_9'], 0)
+
+    signal_cross_0_above = cross_value_from_above(macd_value['MACDs_12_26_9'], 0)
+    signal_cross_0_bottom = cross_value_from_bottom(macd_value['MACDs_12_26_9'], 0)
+
+    value_top_signal = up_down_line(macd_value['MACD_12_26_9'], macd_value['MACDs_12_26_9'])
+    signal_top_value = up_down_line(macd_value['MACDs_12_26_9'], macd_value['MACD_12_26_9'])
+
+    cross_signal_bullish = cross_line_bullish(macd_value['MACD_12_26_9'], macd_value['MACDs_12_26_9'])
+    cross_signal_bearish = cross_line_bearish(macd_value['MACD_12_26_9'], macd_value['MACDs_12_26_9'])
+    d = {
+        'macd_value': macd_value['MACD_12_26_9'],
+        'macd_signal': macd_value['MACDs_12_26_9'],
+        'macd_value_cross_0_above': value_cross_0_above,
+        'macd_value_cross_0_bottom': value_cross_0_bottom,
+        'macd_signal_cross_0_above': signal_cross_0_above,
+        'macd_signal_cross_0_bottom': signal_cross_0_bottom,
+        'macd_value_top_signal': value_top_signal,
+        'macd_signal_top_value': signal_top_value,
+        'macd_cross_signal_bullish': cross_signal_bullish,
+        'macd_cross_signal_bearish': cross_signal_bearish,
+    }
+
+    return create_dataframe(d)

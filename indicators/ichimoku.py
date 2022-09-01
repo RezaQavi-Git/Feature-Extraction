989405d9641ec9620634bb007fc281783ad647eb
Reza Qavi <rezaqavi1378@gmail.com>
2022-09-01 19:01:14 +0430

    ichimoku done

--- indicators/ichimoku.py
+++ indicators/ichimoku.py
@@ -8,6 +8,17 @@ from utils.basic import create_dataframe
 from utils.difference_from_line import difference_from_line
 from utils.sum_in_period import *
 
+
+def cloud_color_detection(close, span_A, span_B):
+    color = [0] * len(close)
+    for i in range(len(close)):
+        if ((span_A[i] > span_B[i]) and (close[i] < span_A[i]) and (close[i] > span_B[i]) ):
+            color[i] = 1
+        elif((span_B[i] > span_A[i]) and (close[i] < span_B[i]) and (close[i] > span_A[i]) ):
+            color[i] = -1
+
+    return color
+
 def ichimoku(df):
     ichimoku_value = ta.ichimoku(df['high'], df['low'], df['close'])
     diff_tenkan_sen_with_close = difference_from_line(ichimoku_value[0]['ITS_9'], df['close'])
@@ -17,7 +28,8 @@ def ichimoku(df):
     diff_span_A_with_span_B = difference_from_line(ichimoku_value[0]['ISA_9'], ichimoku_value[0]['ISB_26'])
     diff_tenkan_with_kijun = difference_from_line(ichimoku_value[0]['ITS_9'], ichimoku_value[0]['IKS_26'])
 
-    # TODO
+
+    cloud_color = cloud_color_detection(df['close'], ichimoku_value[0]['ISA_9'], ichimoku_value[0]['ISB_26'])
     d = {
         'ichimoku_span_A': ichimoku_value[0]['ISA_9'],
         'ichimoku_span_B':  ichimoku_value[0]['ISB_26'],
@@ -28,7 +40,8 @@ def ichimoku(df):
         'ichimoku_diff_tenkan_sen_with_close':  diff_tenkan_sen_with_close,
         'ichimoku_diff_kijun_sen_with_close':  diff_kijun_sen_with_close,
         'ichimoku_diff_span_A_with_span_B':  diff_span_A_with_span_B,
-        'ichimoku_diff_tenkan_with_kijun':  diff_tenkan_with_kijun
+        'ichimoku_diff_tenkan_with_kijun':  diff_tenkan_with_kijun,
+        'ichimoku_price_in_cloud_color':  cloud_color,
     }
 
     return create_dataframe(d)

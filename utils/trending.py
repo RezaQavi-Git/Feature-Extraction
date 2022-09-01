1d3485fd07d3a81ef4d4a112c447d02c41ca667d
Reza Qavi <rezaqavi1378@gmail.com>
2022-09-01 17:47:05 +0430

    Update utils

--- utils/trending.py
+++ utils/trending.py
@@ -1,9 +1,9 @@
 def trend_up(column, period):
-    trend = [0]*period + [1] * (len(column)-period)
-    for i in range(period, len(column)):
-        if min(column[i-period:i+1]) < column[i-period]:
-            trend[i] = 0
-    return trend
+  trend = [0]*period + [1] * (len(column)-period)
+  for i in range(period, len(column)):
+    if min(column[i-period:i+1]) < column[i-period]:
+        trend[i] = 0
+  return trend
 
 def trend_down(column, period):
   trend = [0]*period + [1] * (len(column)-period)

c0f8d56e601bdab99b2527d661a40656a1a6ee67
Reza Qavi <rezaqavi1378@gmail.com>
2022-09-01 10:06:10 +0430

    Fill basic funtions

---
+++ utils/cross_value.py
@@ -0,0 +1,16 @@
+
+def cross_value_from_bottom(column, value):
+    crossed = [0] * len(column)
+    for i in range(1, len(column)):
+        if((round(column[i]) == round(value)) and (column[i-1] < value)):
+            crossed[i] = 1
+
+    return crossed
+
+def cross_value_from_above(column, value):
+  crossed = [0] * len(column)
+  for i in range(1, len(column)):
+    if((round(column[i]) == round(value)) and (column[i-1] > value)):
+      crossed[i] = 1
+
+  return crossed

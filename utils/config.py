6264a5c1c5da73968e59dc9f390a5204fce61b2e
Reza Qavi <rezaqavi1378@gmail.com>
2022-09-04 21:24:31 +0430

    Add get request config

--- utils/config.py
+++ utils/config.py
@@ -10,4 +10,6 @@ DURATION = '5m'
 FILE_PATH = 'data\export_{0}_{1}.csv'
 
 EXPORTS_FOLDER = './extracts'
-WRITE_FILE_PATH = '{0}.csv'
+WRITE_FILE_PATH = 'extracted_{0}_{1}.csv'
+
+GET_URL = 'http://65.21.98.186:8080/api/download/coin?symbol={0}&duration={1}'
